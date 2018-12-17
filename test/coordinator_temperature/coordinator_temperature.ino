b9ce88085182b1ba89a669e51cd892f36912354a
frasuri94 <fra.suri94@gmail.com>
2018-12-17 18:22:28 +0100

    Update coordinator_temperature.ino

--- test/coordinator_temperature/coordinator_temperature.ino
+++ test/coordinator_temperature/coordinator_temperature.ino
@@ -1,3 +1,4 @@
+#include <Ethernet.h>
 #include <XBee.h>
 
 XBeeWithCallbacks xbee;
@@ -8,20 +9,24 @@ XBeeAddress64 coordinator_address = XBeeAddress64(0x0, 0x0);
 void receive(ZBRxResponse& rx, uintptr_t) 
 {
   printPayload(rx);
-  
   Serial.println("");
 }
 
 void printPayload(ZBRxResponse& rx)
 {
-  for (int i = 0; i < rx.getDataLength(); i++) 
-  {
-    Serial.print("Payload: ");
-    //Minus 48 because it's an ASCII value
-    Serial.print(rx.getData(i)-48, DEC);
-  }
+  int temperature = concatenate(rx.getData(0)-48, rx.getData(1)-48);
+  Serial.print(temperature);
   Serial.println("");
 }
+
+unsigned concatenate(unsigned x, unsigned y) 
+{
+    unsigned pow = 10;
+    while(y >= pow)
+        pow *= 10;
+    return x * pow + y;        
+}
+
 void setup()  
 {
   Serial.begin(9600);
