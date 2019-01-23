ef9ab63b73a39b5de7911c995bde9eb75bdb8afb
frasuri94 <fra.suri94@gmail.com>
2019-01-23 11:08:36 +0100

    added router sketch

---
+++ test/router_test/router_test/router_test.ino
@@ -0,0 +1,27 @@
+#include <XBee.h>
+
+XBeeWithCallbacks xbee;
+XBeeAddress64 coordinator_address = 
+  XBeeAddress64(0x0013a200, 0x4079a81f);
+
+void setup()  
+{
+  xbee.onZBRxResponse(receive);
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+  delay(100);
+}
+
+void receive(ZBRxResponse& rx, uintptr_t) 
+{
+  //Message packet
+  ZBTxRequest message = ZBTxRequest(coordinator_address, 
+                                    rx.getFrameData(), 
+                                    rx.getDataLength());
+  xbee.send(message);
+}
+
+void loop()  
+{
+  xbee.loop();
+}
